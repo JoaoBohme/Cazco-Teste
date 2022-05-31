199193720caeadc123a36547906ab0e6b9f076e2
João B. Bohme <joao.bohmecastro@gmail.com>
2022-05-30 22:56:52 -0300

    Subindo rotas e controllers de admin e user.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::namespace('App')->group(function() {
+    Route::get('/', 'Http\Controllers\Site\HomeController');
+    
+    Route::get('admin/login', 'Http\Controllers\Site\Admin\LoginController@index');
+    Route::get('admin/forgot-password', 'Http\Controllers\Site\Admin\ForgotPasswordController@index');
+    Route::get('admin/password-recovery', 'Http\Controllers\Site\Admin\PasswordRecoveryController@index');
+    Route::get('admin/users', 'Http\Controllers\Site\Admin\UsersController@index');
+    Route::get('admin/create', 'Http\Controllers\Site\Admin\CreateController@index');
+    Route::get('admin/edit', 'Http\Controllers\Site\Admin\EditController@index');
+
+    Route::get('user/login', 'Http\Controllers\Site\UserController@index');
+    Route::get('user/forgot-password', 'Http\Controllers\Site\UserController@index');
+    Route::get('user/password-recovery', 'Http\Controllers\Site\UserController@index');
 });
